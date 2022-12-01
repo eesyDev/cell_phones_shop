f69642e0c01b67d4fad82b513815b192da157ed1
Rano Khassanova <ranokhassanova@MacBook-Air-Rano.local>
2022-12-01 18:15:04 +0600

    adding columns

--- products.py
+++ products.py
@@ -18,13 +18,13 @@ def create_table_product():
 
 # create_table_product()
 
-def insert_product(name: str, description: str, price: str, photo: str, color: str):
+def insert_product(name: str, description: str, price: str, photo: str, color: str, brand: str, callback: str):
     query = f"""
     INSERT INTO products (
-        name, description, price, photo, color
+        name:, description, price, photo, color, brand, callback
         )
         VALUES (
-            '{name}', '{description}', {price}, '{photo}', '{color}'
+            '{name}', '{description}', {price}, '{photo}', '{color}', '{brand}', '{callback}
         );"""
     cursor.execute(query=query)
     conn.commit()
